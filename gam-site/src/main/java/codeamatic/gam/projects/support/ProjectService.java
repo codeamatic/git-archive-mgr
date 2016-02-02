fb40eb77caa7ba0c194d74105e87189583d65d7c
Jason Webb <jason.webb@rockfishinteractive.com>
2016-02-02 00:29:24 -0500

    Added documentation for ProjectService

--- gam-site/src/main/java/codeamatic/gam/projects/support/ProjectService.java
+++ gam-site/src/main/java/codeamatic/gam/projects/support/ProjectService.java
@@ -9,6 +9,11 @@ import java.util.List;
 import codeamatic.gam.projects.Project;
 import codeamatic.gam.projects.util.GitUtil;
 
+/**
+ * Service used for interacting with the project repository as well as for
+ * handling functionality directly related to cloning projects from the
+ * repository url.
+ */
 @Service
 public class ProjectService {
 
@@ -22,18 +27,31 @@ public class ProjectService {
     this.projectFormAdapter = projectFormAdapter;
   }
 
+  /**
+   * Retrieve all projects from repository.
+   *
+   * @return list of projects, {@code null} otherwise
+   */
   public List<Project> getProjects() {
     return projectRepository.findAll();
   }
 
+  /**
+   * Retrieve a single project with a project id matching what has been provided.
+   *
+   * @param id Integer project id
+   * @return single project associated with provided id, {@code null} otherwise
+   */
   public Project getProject(Integer id) {
     return projectRepository.findById(id);
   }
 
-  public Project getProjectName(String name) {
-    return projectRepository.findByName(name);
-  }
-
+  /**
+   * Adds project to repository and clones the associated Git repo to the project build directory
+   *
+   * @param projectForm ProjectForm model
+   * @return a new Project object
+   */
   public Project addProject(ProjectForm projectForm) {
     Project project = projectFormAdapter.createProjectFromProjectForm(projectForm);
 
@@ -41,16 +59,30 @@ public class ProjectService {
     File buildDir = new File(project.getBuildDirectory());
 
     if (!buildDir.exists() && !buildDir.mkdirs()) {
+      // TODO: throw an exception of some sort
       System.out.println("Unable to create project directory");
     }
 
-    cloneProject(project.getRepoUrl(), project.getBuildDirectory());
+    boolean cloned = cloneProject(project.getRepoUrl(), project.getBuildDirectory());
 
-    projectRepository.save(project);
+    // ONLY save the project if the cloning worked as expected.
+    // TODO: throw exception if unable to clone
+    if(cloned) {
+      projectRepository.save(project);
+    }
+
     return project;
   }
 
+  /**
+   * Clones the project from the Git repository into the projects build directory.
+   *
+   * @param repoUrl String the url of the projects repository
+   * @param buildDir String the directory location where the project will be built
+   * @return true if the project is cloned correctly, false otherwise
+   */
   private boolean cloneProject(String repoUrl, String buildDir) {
+    // TODO: throw an uncloneable exception or something
     String gitClone = "git clone " + repoUrl + " .";
 
     Process cloneProcess = GitUtil.processCommand(gitClone, buildDir);
