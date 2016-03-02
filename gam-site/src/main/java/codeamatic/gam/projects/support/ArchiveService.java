d213e7a3cefa6a21f0a349b409d2bb3a5addc995
Jason Webb <jason@jswebb.net>
2016-03-02 13:01:05 -0500

    - Refactored the ArchiveService into several smaller methods - Updated the application.properties file so that the username and password for the db is more generic

--- gam-site/src/main/java/codeamatic/gam/projects/support/ArchiveService.java
+++ gam-site/src/main/java/codeamatic/gam/projects/support/ArchiveService.java
@@ -38,6 +38,8 @@ public class ArchiveService {
 
   private final ArchiveFormAdapater archiveFormAdapater;
 
+  private String readmeTxt;
+
   @Autowired
   public ArchiveService(ArchiveFormAdapater archiveFormAdapater) {
     this.archiveFormAdapater = archiveFormAdapater;
@@ -74,125 +76,68 @@ public class ArchiveService {
       } else {
         // add to a separate list used for various files that weren't
         // part of the app or web list.  This could be .xml files, config files, source code, etc.
+        // purpose of this list is more for logging/outputting additional information
         otherList.add(line);
       }
     }
 
-    return generateZips(appList, webList, archive, project);
+    return generateZips(appList, webList, archive);
   }
 
   /**
-   * Process and generate zip archives for
+   * Process and generate zip archive, returning a path where the zip file exists
+   *
+   * @param appList List of strings that identify modified application related files
+   * @param webList List of strings that identify modified web/static related files
+   * @param archive Archive object representing a new archive
+   * @return a generated zip path, or null
+   * @throws IOException
    */
-  private String generateZips(List<String> appList, List<String> webList, Archive archive,
-                              Project project)
+  private String generateZips(List<String> appList, List<String> webList, Archive archive)
       throws IOException {
 
     // Do nothing if the files haven't changed
     if (appList.isEmpty() && webList.isEmpty()) {
-      System.out.println("No files have changed.");
+      logger.info("There is no difference between {} and  {}.", archive.getDiffParam1(), archive.getDiffParam2());
       return null;
     }
 
+    Project project = archive.getProject();
     String fromAuthor = "From_" + project.getOwner();
 
     // Create temp directory used for building archive
-    Path
-        tempDir =
-        Files.createTempDirectory(project.getName().toLowerCase());
-    Path prjStructureOut;
+    Path tempDir = Files.createTempDirectory(project.getName().toLowerCase());
+
+    if(! Files.exists(tempDir)) {
+      logger.error("Error creating a temporary directory.");
+      // TODO: throw a custom exception
+      return null;
+    }
 
     Date date = new Date();
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
 
-    if (Files.exists(tempDir)) {
-      prjStructureOut =
+    // Create approved directory structure From_X => Project Name => Date => etc
+    Path prjStructureOut =
           Paths.get(
               tempDir + "/" + fromAuthor + "/" + project.getName()
               + "/" + simpleDateFormat.format(date));
-      Files.createDirectories(prjStructureOut);
-    } else {
-      // kill it...
-      System.out.println("ERROR creating temporary directory.");
-      return null;
-    }
+    Files.createDirectories(prjStructureOut);
 
     final Map<String, String> env = new HashMap<>();
     env.put("create", "true");
 
-    String readmeTxt = archive.getReadmeTxt();
+    readmeTxt = archive.getReadmeTxt();
+    String projectStr = prjStructureOut.toString();
 
-    if (!webList.isEmpty()) {
-      URI
-          uri =
-          URI.create("jar:file:/" + prjStructureOut.toString().replace("\\", "/") + "/web.zip");
-
-      readmeTxt += "\r\n" + "# Web Updates" + "\r\n";
-
-      try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
-
-        for (String file : webList) {
-          Path source = Paths.get(project.getBuildDirectory() + "/" + file);
-
-          if (Files.notExists(source)) {
-            // File was deleted, log it to README
-            System.out.println("File was removed " + source.toString());
-            continue;
-          }
-
-          // replace the prefix
-          file = file.replace(archive.getWebPrefix(), "");
-
-          readmeTxt += file + "\r\n";
-
-          Path target = zipfs.getPath(file);
-          Path parentTargetDir = target.getParent();
-
-          if (parentTargetDir != null && !Files.exists(parentTargetDir)) {
-            Files.createDirectories(parentTargetDir);
-          }
-
-          Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
-        }
-      }  catch(IOException ex) {
-        logger.error("Unable to create web.zip - {}", ex.getMessage());
-      }
+    // Build WebList
+    if(! webList.isEmpty()) {
+      buildFileList(webList, archive, env, projectStr, "web");
     }
 
-    if (!appList.isEmpty()) {
-      URI
-          uri =
-          URI.create("jar:file:/" + prjStructureOut.toString().replace("\\", "/") + "/app.zip");
-
-      readmeTxt += "\r\n" + "# App Updates" + "\r\n";
-
-      try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
-
-        for (String file : appList) {
-          Path source = Paths.get(project.getBuildDirectory() + "/" + file);
-
-          if (Files.notExists(source)) {
-            // File was deleted, log it to README
-            System.out.println("File was removed " + source.toString());
-            continue;
-          }
-
-          file = file.replace(archive.getAppPrefix(), "");
-
-          readmeTxt += file + "\r\n";
-
-          Path target = zipfs.getPath(file);
-          Path parentTargetDir = target.getParent();
-
-          if (parentTargetDir != null && !Files.exists(parentTargetDir)) {
-            Files.createDirectories(parentTargetDir);
-          }
-
-          Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
-        }
-      }  catch(IOException ex) {
-        logger.error("Unable to create app.zip - {}", ex.getMessage());
-      }
+    // Build AppList
+    if(! appList.isEmpty()) {
+      buildFileList(appList, archive, env, projectStr, "app");
     }
 
     // Write readme data to README txt file
@@ -203,23 +148,86 @@ public class ArchiveService {
     }
 
     // Create final zip
-    URI
-        uri =
-        URI.create("jar:file:/" + tempDir.toString().replace("\\", "/") + "/" + fromAuthor + "_"
-                   + project.getName() + ".zip");
+    URI uri = URI.create("jar:file:/" + tempDir.toString().replace("\\", "/") + "/" + fromAuthor + "_" + project.getName() + ".zip");
 
-    // Walk the file source and build a zip from it
-    String zipPath;
+    logger.info(readmeTxt);
+
+    return generateZipPath(env, uri, tempDir.toString(), fromAuthor);
+  }
+
+  /**
+   *
+   * @param fileList List of string identifying web or application files that have been modified
+   * @param archive Archive object representing the current archive
+   * @param env Map listing of properties to be passed for file creation
+   * @param projectStructure String representing temporary directory structure to be used in the zip packaging
+   * @param zipFilename The name of the zip file to be created
+   */
+  private void buildFileList(List<String> fileList, Archive archive, Map<String, String> env, String projectStructure, String zipFilename) {
+    Project project = archive.getProject();
+
+    // remove leading forward slash if applicable
+    if(projectStructure.charAt(0) != '/') {
+      projectStructure = "/" + projectStructure;
+    }
+
+    URI uri = URI.create("jar:file:" + projectStructure.replace("\\", "/") + "/" + zipFilename + ".zip");
+
+    readmeTxt += "\r\n" + "# Web Updates" + "\r\n";
+
+    try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+
+      for (String file : fileList) {
+        Path source = Paths.get(project.getBuildDirectory() + "/" + file);
+
+        if (Files.notExists(source)) {
+          // File was deleted, log it to README
+          logger.info("File [{}] was deleted from branch/commit.", source.toString());
+          continue;
+        }
+
+        // replace the prefixes
+        file = file.replace(archive.getWebPrefix(), "");
+        file = file.replace(archive.getAppPrefix(), "");
+
+        readmeTxt += file + "\r\n";
+
+        Path target = zipfs.getPath(file);
+        Path parentTargetDir = target.getParent();
+
+        if (parentTargetDir != null && !Files.exists(parentTargetDir)) {
+          Files.createDirectories(parentTargetDir);
+        }
+
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+      }
+    }  catch(IOException ex) {
+      logger.error("Unable to create web.zip - {}", ex.getMessage());
+    }
+  }
+
+  /**
+   * Generate zip path to be used for creating a zip file
+   *
+   * @param env Map listing of properties to be passed for file creation
+   * @param uri URI path for the zip file structure
+   * @param sourceDir String of temporary directory location
+   * @param projectAuthor String representing the author of the project
+   * @return String zip path
+   */
+  private String generateZipPath(Map<String, String> env, URI uri, String sourceDir, String projectAuthor) {
+    String zipPath = "";
 
     try (FileSystem zipFinal = FileSystems.newFileSystem(uri, env)) {
-      Path source = Paths.get(tempDir + "/" + fromAuthor);
-      Path target = zipFinal.getPath(fromAuthor);
+      Path source = Paths.get(sourceDir + "/" + projectAuthor);
+      Path target = zipFinal.getPath(projectAuthor);
 
       Files.walkFileTree(source, new CopyFileVisitor(target));
       zipPath = zipFinal.toString();
+    } catch(IOException ex) {
+       logger.error("Unable to create new File System for zip - {}", ex.getMessage());
     }
 
-    System.out.println(readmeTxt);
     return zipPath;
   }
 
@@ -238,7 +246,7 @@ public class ArchiveService {
 
     // check for existence of project Dir
     if (!Files.exists(Paths.get(projectDir))) {
-      System.out.println("Path doesn't exist");
+      logger.error("The project directory [{}] doesn't exist.", projectDir);
     }
 
     // fetch/update entire repo
